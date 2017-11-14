d84649070a16d107ce277661e7d381784116e508
Aler Denisov <aler.zampillo@gmail.com>
2017-11-14 11:43:38 +0300

    add BULK_SIZE variable

--- phases/prepareMintDataPhase.js
+++ phases/prepareMintDataPhase.js
@@ -3,9 +3,9 @@ import Phase from './basePhase'
 export default class PrepareMintDataPhase extends Phase {
   static get key() { return "mint-data" }
 
-  static async execute({ state, db, log, MINT_STATES }) {
+  static async execute({ state, db, log, MINT_STATES, CONSTANTS }) {
     log.info('Slice to mint')
-    const mintSlice = db.get('accounts').filter({ state: MINT_STATES.NONE }).take(50).value()
+    const mintSlice = db.get('accounts').filter({ state: MINT_STATES.NONE }).take(CONSTANTS.bulkSize).value()
 
     state.mintData = mintSlice.reduce((acc, el) => {
       acc.accounts.push(el.wallet)
